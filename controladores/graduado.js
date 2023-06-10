efb6b4d3ff222e6dc922d8e0e70f097ce427761e
Daniello2301 <daniel.lopera@est.iudigital.edu.co>
2023-06-10 18:11:38 -0500

    creadon controlador y rutas graduado

--- controladores/graduado.js
+++ controladores/graduado.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const Graduado = require('../modelos/Graduado');
 
 const getGraduados = async(req, res) => {
@@ -15,7 +16,106 @@ const getGraduados = async(req, res) => {
     }
 }
 
+const getById = async(req, res) => {
+    try {
+        
+        const { id } = req.params;
+
+        const graduadoEncontrado = await Graduado.findById({ _id: id });
+        if(graduadoEncontrado){
+            return res.status(200).json({ msg: "Graduado encontrado", gradudo: graduadoEncontrado });
+        }else{
+            return res.status(404).json({ msg: "Graduado no econtrado" });
+        }
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({error});
+    }
+}
+
+const createGraduado = async(req, res) => {
+    try {
+        
+        const { ...data } = req.body;
+
+        const graduadoExistente = await Graduado.findOne({ cedula: data.cedula });
+        if(graduadoExistente){
+            return res.status(400).json({ msg: "El graduado ya existe"});
+        }
+
+        let nuevoGraduado = new Graduado();
+        
+        nuevoGraduado.cedula = data.cedula;
+        nuevoGraduado.nombreCompleto = data.nombreCompleto;
+        nuevoGraduado.fechaNacimiento = data.fechaNacimiento || "0000-00-00T00:00:00.000Z";
+
+        nuevoGraduado = await nuevoGraduado.save();
+
+        return res.status(200).json({ msg: "El graduado se guardo correctamtene", graduado: nuevoGraduado});
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({msg:"Error interno del servidor", error: error})
+    }
+}
+
+const updateGraduado = async(req, res) => {
+    try {
+        
+        const { id } = req.params;
+
+        let gradudoEncontrado = await Graduado.findById({ _id: id });
+        if(!gradudoEncontrado){
+            return res.status(404).json({ msg: "Graduado no encontrado"});
+        }
+
+        const { cedula, nombreCompleto, fechaNacimiento } = req.body;
+
+        const graduadoExistente = await Graduado.findOne({ cedula: cedula, _id: {$ne: id}});
+        if(graduadoExistente){
+            return res.status(400).json({ msg: "El graduado ya existe"});
+        }
+        
+        /* if(cedula){
+            gradudoEncontrado.cedula = cedula;
+        }else{
+            gradudoEncontrado.cedula = gradudoEncontrado.cedula;            
+        } */
+        gradudoEncontrado.cedula = cedula ? cedula : gradudoEncontrado.cedula;
+        gradudoEncontrado.nombreCompleto = nombreCompleto ? nombreCompleto : gradudoEncontrado.nombreCompleto;
+        gradudoEncontrado.fechaNacimiento = fechaNacimiento ? fechaNacimiento : gradudoEncontrado.fechaNacimiento ;
+
+        gradudoEncontrado = await gradudoEncontrado.save();
+
+        return res.status(200).json({ msg: "El graduado se actualizo correctamente", graduado: gradudoEncontrado});
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ msg: "Internal server error!" });
+    }
+}
+
+const deleteGraduado = async(req, res) => {
+    try {
+        
+        const { id } = req.params;
+        
+        const response = await Graduado.findByIdAndDelete(id);
+        
+        return res.status(200).json({ msg: "Graduado eliminado correctamente!", data: response});
+
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ msg: "Internal server error!" });
+    }
+}
 
 module.exports ={
-    getGraduados
+    getGraduados,
+    getById,
+    createGraduado,
+    updateGraduado,
+    deleteGraduado,
 }
--- rutas/graduados.js
+++ rutas/graduados.js
@@ -3,8 +3,18 @@ const controladorGraduados = require('../controladores/graduado');
 
 const router = Router();
 
+// listar todo y por Id
 router.get('/graduados', controladorGraduados.getGraduados);
+router.get('/graduado/:id', controladorGraduados.getById);
 
 
+// crear graduado
+router.post('/create/graduado', controladorGraduados.createGraduado);
+
+// actualizar graduado
+router.put('/update/graduado/:id', controladorGraduados.updateGraduado);
+
+// Borrar gradudo
+router.delete('/delete/graduado/:id', controladorGraduados.deleteGraduado);
 
 module.exports = router;
