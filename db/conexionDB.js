f98ebbed12633eb66d061133815c789314a4fef2
Daniel Lopera Gómez <92329281+Daniello2301@users.noreply.github.com>
2024-06-26 13:45:06 -0500

    docs(database): add documentation to database connection

--- db/conexionDB.js
+++ db/conexionDB.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-const { MONGO_URI, MONGO_URI_TEST, NODE_ENV} = process.env
+/**
+ * Establishes a connection to MongoDB based on the environment.
+ */
+const conexion = async () => {
+    // Determine which MongoDB URI to use based on the environment
+    const { MONGO_URI, MONGO_URI_TEST, NODE_ENV } = process.env;
+    const URI = NODE_ENV === 'test' ? MONGO_URI_TEST : MONGO_URI;
 
-const URI = NODE_ENV === 'test' 
-    ? MONGO_URI_TEST
-    : MONGO_URI  
-
-const conexion = async() => {
-
-    console.log('conectando a mogodb...');
-    try {        
+    console.log('Connecting to MongoDB...');
+    try {
+        // Connect to MongoDB using Mongoose
         await mongoose.connect(URI);
-        console.log('conectado correctamente a mongodb');
+        console.log('Connected to MongoDB successfully');
     } catch (error) {
-        console.log(error);
+        console.error('MongoDB connection error:', error.message);
     }
 }
 
